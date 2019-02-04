8c5e6a50311d23e78d4358a3e59c5cb1854afe22
takik <tarak.akik@gmail.com>
2019-02-04 20:38:13 +0100

    create spring cloud project

---
+++ src/main/java/com/takik/springcloudintro/SpringCloudIntroApplication.java
@@ -0,0 +1,14 @@
+package com.takik.springcloudintro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringCloudIntroApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringCloudIntroApplication.class, args);
+    }
+
+}
+
